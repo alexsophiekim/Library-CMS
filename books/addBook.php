aa1f7cf184a5fe98d35da0e4bc23b798dfedd88a
Sophie <48259684+alexsophiekim@users.noreply.github.com>
2019-10-11 12:08:20 +1300

    test before start homework

--- books/addBook.php
+++ books/addBook.php
@@ -51,14 +51,14 @@
             $safeYear = mysqli_real_escape_string($dbc, $year);
             $safeDescription = mysqli_real_escape_string($dbc, $description);
 
-            // $sql = "INSERT INTO `authors`(`name`) VALUES ('$safeAuthor')";
-            // $result = mysqli_query($dbc, $sql);
-            // if ($result && mysqli_affected_rows($dbc) > 0) {
-            //     $authorID = $dbc->insert_id;
-            // } else {
-            //     dis('Something went wrong with adding in our author');
-            // }
-            $authorID = 1;
+            $sql = "INSERT INTO `authors`(`name`) VALUES ('$safeAuthor')";
+            $result = mysqli_query($dbc, $sql);
+            if ($result && mysqli_affected_rows($dbc) > 0) {
+                $authorID = $dbc->insert_id;
+            } else {
+                dis('Something went wrong with adding in our author');
+            }
+            // $authorID = 1;
             $booksSql = "INSERT INTO `books`(`title`, `year`, `description`, `author_id`) VALUES ('$safeTitle',$safeYear,'$safeDescription',$authorID)";
             $booksResult = mysqli_query($dbc, $booksSql);
             if ($booksResult && mysqli_affected_rows($dbc) > 0) {
