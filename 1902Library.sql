907afe6ce981853072aa283eac5548f1f37eb0b5
Sophie Kim <270010565@bun2-206-23.local>
2019-10-11 13:38:26 +1300

    update SQL file:

--- 1902Library.sql
+++ 1902Library.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 09, 2019 at 10:54 PM
+-- Generation Time: Oct 11, 2019 at 12:37 AM
 -- Server version: 5.7.27-0ubuntu0.16.04.1
 -- PHP Version: 7.0.33-8+ubuntu16.04.1+deb.sury.org+1
 
@@ -38,7 +38,8 @@ CREATE TABLE `authors` (
 INSERT INTO `authors` (`_id`, `name`) VALUES
 (1, 'test author'),
 (2, 'j k rowing'),
-(3, 'j k rowing');
+(3, 'j k rowing'),
+(4, 'testauthor');
 
 -- --------------------------------------------------------
 
@@ -53,6 +54,47 @@ CREATE TABLE `books` (
   `description` text NOT NULL,
   `author_id` tinyint(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `books`
+--
+
+INSERT INTO `books` (`_id`, `title`, `year`, `description`, `author_id`) VALUES
+(1, 'testbook', 2002, 'this is test book ', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `directors`
+--
+
+CREATE TABLE `directors` (
+  `_id` tinyint(6) NOT NULL,
+  `name` varchar(100) CHARACTER SET utf8mb4 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `directors`
+--
+
+INSERT INTO `directors` (`_id`, `name`) VALUES
+(1, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `movies`
+--
+
+CREATE TABLE `movies` (
+  `_id` tinyint(6) UNSIGNED NOT NULL,
+  `title` varchar(100) CHARACTER SET utf8mb4 NOT NULL,
+  `year` year(4) NOT NULL,
+  `description` text CHARACTER SET utf8mb4 NOT NULL,
+  `director` varchar(100) CHARACTER SET utf8mb4 NOT NULL,
+  `length` tinyint(3) NOT NULL,
+  `genre` varchar(100) CHARACTER SET utf8mb4 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
@@ -72,6 +114,18 @@ ALTER TABLE `books`
   ADD KEY `author_id` (`author_id`);
 
 --
+-- Indexes for table `directors`
+--
+ALTER TABLE `directors`
+  ADD PRIMARY KEY (`_id`);
+
+--
+-- Indexes for table `movies`
+--
+ALTER TABLE `movies`
+  ADD PRIMARY KEY (`_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -79,11 +133,21 @@ ALTER TABLE `books`
 -- AUTO_INCREMENT for table `authors`
 --
 ALTER TABLE `authors`
-  MODIFY `_id` tinyint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `_id` tinyint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `books`
 --
 ALTER TABLE `books`
+  MODIFY `_id` tinyint(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `directors`
+--
+ALTER TABLE `directors`
+  MODIFY `_id` tinyint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `movies`
+--
+ALTER TABLE `movies`
   MODIFY `_id` tinyint(6) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- Constraints for dumped tables
